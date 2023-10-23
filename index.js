64b3b36726e6642313890639a0a40b5176a1166e
Michael Angelo F. Dino <32347131+keldino@users.noreply.github.com>
2023-10-23 15:44:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -627,7 +627,7 @@ var postOnce = false;
                     var actualDistance =
                         this.distanceMeter.getActualDistance(Math.ceil(this.distanceRan));
                         getDistance = actualDistance;
-                        if(actualDistance<10){
+                        if(actualDistance<1000){
                            this.horizon.update(deltaTime, this.currentSpeed, hasObstacles,
                         this.inverted);
                         }
